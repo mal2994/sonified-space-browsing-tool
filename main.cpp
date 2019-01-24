e4c0bd153f4f3fd56258cdb4d599f7513c343eb6
mal2994 <46338491+mal2994@users.noreply.github.com>
2019-01-24 14:45:54 -0500

    commit 2: disc
    first implementation disc as a child class of user.

--- main.cpp
+++ main.cpp
@@ -7,24 +7,40 @@ using namespace std;
 
 int main(void)
 {
-	// user teleport
+	//create user
 	User User1;
-	cout << "User Initialized." << endl;
+	cout << "User Created." << endl;
 	cout << "coords: " << User1.getTrueX() << ", " << User1.getTrueY() << endl;
+	
+	//create rabbit array for the world
+	Rabbit AllRabbits[10];
+
+	// user teleport
 	User1.setTrueX(5);
 	User1.setTrueY(10);
 	cout << "User teleported." << endl;
 	cout << "coords: " << User1.getTrueX() << ", " << User1.getTrueY() << endl;
 
-	// rabbit create
+	// new rabbit
 	Rabbit Rabbit1;
+	AllRabbits[0] = Rabbit1;
 	Rabbit1.setTrueX(10);
 	Rabbit1.setTrueY(20);
 	Rabbit1.calcDistanceToUser(User1.getTrueX(), User1.getTrueY());
-	cout << "Rabbit1 Initialized." << endl;
+	cout << "Rabbit1 Created." << endl;
 	cout << "coords: " << Rabbit1.getTrueX() << ", " << Rabbit1.getTrueY() << endl;
 	cout << "distance to user: " << Rabbit1.getDistanceToUser() << endl;
 
+	// create disc
+	User1.getDisc1().setRadius1(2);
+	User1.getDisc1().setRadius2(8);
+	User1.getDisc1().activate(true);
+	cout << "Disc created." << endl;
+	cout << "radius1: " << User1.getDisc1().getRadius1() << endl;
+	cout << "radius2: " << User1.getDisc1().getRadius2() << endl;
+
+	/* this is just a test */
+	//User1.activateInsideRabbits(AllRabbits[]);
 
 	return 0;
 }
--- user.cpp
+++ user.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include "user.h"
+#include "rabbit.h"
 
 using namespace std;
 
-// CONSTRUCTOR
+/* CONSTRUCTOR */
 User::User()
 {
     truex = 0;
    	truey = 0;
 }
 
-// MODIFICATION MEMBER FUNCTIONS
+/* MODIFICATION MEMBER FUNCTIONS */
 void User::setTrueX(int a)
 {
 	truex = a;
@@ -21,7 +22,7 @@ void User::setTrueY(int a)
 	truey = a;
 }
 
-// CONSTANT MEMBER FUNCTIONS
+/* CONSTANT MEMBER FUNCTIONS */
 int User::getTrueX()
 {
 	return truex;
@@ -30,4 +31,52 @@ int User::getTrueX()
 int User::getTrueY()
 {
 	return truey;
+}
+/*
+void User::activateInsideRabbits(Rabbit AllRabbits[])
+{
+	// algorithm coming soon
+	AllRabbits[0].activate(true);
+}
+*/
+Disc User::getDisc1()
+{
+	return Disc1;
+}
+
+Disc::Disc()
+{
+	radius1 = 0;
+	radius2 = 0;
+	activated = 0;
+}
+
+void Disc::activate(bool flag)
+{
+	activated = flag;
+}
+
+void Disc::setRadius1(int a)
+{
+	radius1 = a;
+}
+
+void Disc::setRadius2(int a)
+{
+	radius2 = a;
+}
+
+int Disc::getRadius1()
+{
+	return radius1;
+}
+
+int Disc::getRadius2()
+{
+	return radius2;
+}
+
+bool Disc::isActivated()
+{
+	return activated;
 }
